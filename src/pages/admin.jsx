c559ef572d986ad9ed176dd4d7343d89eb6ed21e
Kev <bichito03052005@gmail.com>
2024-07-08 12:22:12 -0500

    Conf(Frontend): Add upload video function (Admin.jsx)

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "../components/Sidebar/sidebar";
@@ -9,19 +9,26 @@ import AssessmentIcon from "@mui/icons-material/Assessment";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Button, Divider } from "@mui/material";
+import { Button, Divider, Snackbar, TextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import "../assets/styles/admin.css";
+import axios from "axios";
+import Slide from "@mui/material/Slide";
+import { UserContext } from "../context/UserContext";
 
 export default function Admin() {
+  const { id } = useContext(UserContext);
+  const urlPost = "http://localhost:3000/video/create";
   const nav = useNavigate();
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+  const [snackOpen, setSnackOpen] = useState(false);
   const [file, setFile] = useState(null);
+  const [name_video, setVideoName] = useState("");
   const [preview, setPreview] = useState("");
 
   const VisuallyHiddenInput = styled("input")({
@@ -67,10 +74,51 @@ export default function Admin() {
     setPreview(URL.createObjectURL(selectedFile));
   };
 
-  const handleClose = () => {
+  const handleCloseModal = () => {
     setModalOpen(false);
     setFile(null);
     setPreview("");
+    setSidebarOpen(false);
+  };
+
+  const handleCloseSnack = () => {
+    setSnackOpen(false);
+  };
+
+  // Validación y subida del video
+  const validate = () => {
+    if (!file) {
+      console.log("Video no cargado");
+      return;
+    } else if (!name_video) {
+      console.log("Especifica el nombre del video");
+      return;
+    }
+    const params = new FormData();
+    params.append("name_video", name_video);
+    params.append("client_id", id);
+    params.append("video", file);
+
+    uploadVideo(params);
+  };
+
+  const uploadVideo = async (params) => {
+    await axios
+      .post(urlPost, params, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then(function (respuesta) {
+        console.log("Subido con éxito");
+        document.getElementById("btnClose").click();
+        setSnackOpen(true);
+      })
+      .catch(function (error) {
+        console.log(
+          `El video ${name_video} no se subió por el siguiente error: ${error}`
+        );
+      });
   };
 
   return (
@@ -158,7 +206,7 @@ export default function Admin() {
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={modalOpen}
-        onClose={handleClose}
+        onClose={handleCloseModal}
         closeAfterTransition
         slots={{ backdrop: Backdrop }}
         slotProps={{
@@ -195,6 +243,16 @@ export default function Admin() {
             </label>
             {file && (
               <div>
+                <TextField
+                  required
+                  id="outlined-basic"
+                  onChange={(e) => {
+                    setVideoName(e.target.value);
+                  }}
+                  label="Introduce el nombre del video"
+                  variant="outlined"
+                  sx={{ marginTop: "10px", width: "100%" }}
+                />
                 <video
                   width="100%"
                   controls
@@ -205,10 +263,7 @@ export default function Admin() {
                   component="label"
                   variant="contained"
                   color="secondary"
-                  onClick={() => {
-                    // Aquí puedes añadir la lógica para subir el archivo al servidor
-                    alert("Archivo subido exitosamente!");
-                  }}
+                  onClick={validate}
                   sx={{ mt: 2 }}
                 >
                   Subir Archivo
@@ -216,10 +271,11 @@ export default function Admin() {
               </div>
             )}
             <Button
+              id="btnClose"
               component="label"
               variant="contained"
               color="secondary"
-              onClick={handleClose}
+              onClick={handleCloseModal}
               sx={{ mt: 2 }}
             >
               Cerrar
@@ -227,6 +283,14 @@ export default function Admin() {
           </Box>
         </Fade>
       </Modal>
+      <Snackbar
+        open={snackOpen}
+        autoHideDuration={3000}
+        TransitionComponent={Slide}
+        onClose={handleCloseSnack}
+        message={`Video ${name_video} subido exitosamente`}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      />
     </div>
   );
 }
